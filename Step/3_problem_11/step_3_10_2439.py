2e7e0a137066e367ee799462e3b008e55b2ce889
masinogns <masinogns@gmail.com>
2020-02-16 11:53:21 +0900

    Add step 3, problem 11, Scuccess ! Step For problem Finish

---
+++ Step/3_problem_11/step_3_10_2439.py
@@ -0,0 +1,14 @@
+def func_print_star(in_num=0):
+    if in_num >= 1 and in_num <= 100:
+        variable = [" "*(in_num-x)+"*"*x for x in range(1, in_num+1)]
+        for value in variable:
+            print(value)
+    else:
+        print("in_num 범위가 아닙니다.")
+
+def func_main():
+    a = int(input())
+    func_print_star(a)
+
+if __name__ == '__main__':
+    func_main()
